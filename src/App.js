0c816255f782b34b3a1e22b98fb2d14afe9d4e05
theglox <diegoglox@gmail.com>
2021-07-09 16:21:27 -0500

    Controlled Forms

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, {Component} from 'react';
-import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css'; 
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
+import {BrowserRouter} from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+
   render() {
     return (
-      <div >
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}/>
-      </div>
+      <BrowserRouter>
+        <div >
+          <Main />
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,66 +1,48 @@
-import { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-    class Menu extends Component {
+import React from 'react';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle, Breadcrumb,BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
-        constructor(props) {
-            super(props);
-    
-            this.state = {
-                selectedDish: null
-            }
-        }
-    
-        onDishSelect(dish) {
-            this.setState({ selectedDish: dish});
-        }
-    
-        renderDish(dish) {
-            if (dish != null)
-                return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                          <CardTitle>{dish.name}</CardTitle>
-                          <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            else
-                return(
-                    <div></div>
-                );
-        }
-    
-        render() {
-            const menu = this.props.dishes.map((dish) => {
-                return (
-                  <div  className="col-12 col-md-5 m-1">
-                    <Card key={dish.id}
-                      onClick={() => this.onDishSelect(dish)}>
-                      <CardImg width="100%" src={dish.image} alt={dish.name} />
-                      <CardImgOverlay>
-                          <CardTitle>{dish.name}</CardTitle>
-                      </CardImgOverlay>
-                    </Card>
-                  </div>
-                );
-            });
-    
+    function RenderMenuItem ({dish}) {
+        return (
+            <Card>
+                <Link to={`/menu/${dish.id}`}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+                </Link>
+            </Card>
+        );
+    }
+
+    const Menu = (props) => {
+
+        const menu = props.dishes.map((dish) => {
             return (
-                <div className="container">
-                    <div className="row">
-                        {menu}
-                    </div>
-                    <div className="row">
-                      <div  className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                      </div>
-                    </div>
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish}  />
                 </div>
             );
-        }
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>    
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
     }
-    
- 
+
 export default Menu;
